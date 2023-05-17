b82698618a44c7bf4a4e46068bc321b7e4818601
Jordan <jordanmctaggart7@hotmail.co.uk>
2023-05-17 19:31:25 +0100

    Impletemented the 'Edit' button

--- venv/gui.py
+++ venv/gui.py
@@ -4,10 +4,13 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todolist',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
 
 # list of py.simplegui object instances
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -19,6 +22,19 @@ while True:
         new_todo = values['todo'] + "\n"
         todos.append(new_todo)
         functions.write_todos(todos)
+        window['todos'].update(values=todolist)
+
+    elif "Edit":
+        todolist = values['todos'][0]
+        new_todo = values['todo']
+
+        todolist = functions.get_todos()
+        index = todos.index(todo_to_edit)
+        todolist[index] = new_todo
+        functions.write_todos(todolist)
+        window['todos'].update(values=todolist)
+    elif 'todolist':
+        window['todos'].update(values=values['todos'][0])
     elif sg.WIN_CLOSED:
         break
 
